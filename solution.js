986f1d359687dcc72a6ebfd6f77a4431691b0132
Gift13-07 <giftchinwendu07@gmail.com>
2022-07-31 18:49:44 +0600

    git commit

---
+++ solution.js
@@ -0,0 +1,10 @@
+//I am creating an object with two properties/keys//
+let cat={
+    name:'Charlie',
+    age: 10
+}
+//updating new property/key//
+cat.family='Felidae'
+//changing the value of an existing property or key//
+cat.name='Leo'
+console.log(cat)
